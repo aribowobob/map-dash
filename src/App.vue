beffb490f859946634b66fb4b358058119b6ad6f
ananech <anna.nech49@gmail.com>
2018-05-22 22:27:18 +0700

    starting create navbar and top menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,147 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <vue-progress-bar>
+    </vue-progress-bar>
+
+    <v-navigation-drawer class="blue lighten-5" width="250"  light fixed :mini-variant.sync="mini" v-model="drawer" app>
+      <!-- mini-variant.sync="true" -->
+      <v-list class="pa-0">
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://dummyimage.com/60x60/000000/fff.jpg&text=map">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{user.firstName}} {{user.lastName}}</v-list-tile-title>
+          </v-list-tile-content>
+         <v-spacer></v-spacer>
+          <v-list-tile-action style="min-width:30px;">
+          <v-menu bottom right offset-y origin="bottom right" transition="v-slide-y-transition">
+            <v-btn icon light slot="activator">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+            <v-list>
+              <v-list-tile v-for="item in userMenus" :key="item.title" value="true" :to="item.link" router>
+                <v-list-tile-title v-text="item.title"></v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+
+          </v-list-tile-action >
+          <v-list-tile-action style="min-width:30px;">
+            <v-btn icon @click.native.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+
+      </v-list>
+      <v-list>
+        <v-list-tile v-for="item in items" :key="item.title" @click="clickMenu(item)" router>
+          <v-list-tile-action class="pr-1 pl-2 mr-1">
+            <v-icon :class="activeMenuItem.includes(item.title)?'blue--text': ''" :title="item.title"  light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content :class="activeMenuItem.includes(item.title)?'blue--text': ''">
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app="">
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <div class="text-xs-center pr-3">
+      <v-badge left="">
+          <span slot="badge">6</span>
+          <v-icon large color="grey lighten-1">shopping_cart</v-icon>
+        </v-badge>
+
+        <v-badge color="red">
+          <span slot="badge">!</span>
+          <v-icon large color="grey">mail</v-icon>
+        </v-badge>
+      </div>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout>
+          <v-flex row="">
+            <router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      dialog: false,
+      mini: false,
+      dialogText: "",
+      dialogTitle: "",
+      isRootComponent: true,
+      // clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: "dashboard",
+          title: "Dashboard",
+          vertical: "Dashboard",
+          link: "dashboard"
+        },
+        {
+          icon: "shopping_cart",
+          title: "Orders",
+          vertical: "Order",
+          link: "orders"
+        },
+        {
+          icon: "perm_identity",
+          title: "Customers",
+          vertical: "Customer",
+          link: "customers"
+        },
+        {
+          icon: "bubble_chart",
+          title: "Products",
+          vertical: "Product",
+          link: "products"
+        },
+        {
+          icon: "thumbs_up_down",
+          title: "About",
+          vertical: "About",
+          link: "about"
+        }
+      ],
+      userMenus: [
+        {
+          icon: "bubble_chart",
+          title: "Logout",
+          link: "/logout"
+        },
+        {
+          icon: "bubble_chart",
+          title: "Change Password",
+          link: "/changepassword"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      menuItem: "Orders",
+      user: {
+        firstName: 'Lorem',
+        lastName: 'Ipsum'
+      },
+      activeMenuItem: 'Dashboard'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import Vuetify from 'vuetify';
+import router from './router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
+import VueProgressBar from 'vue-progressbar';
+const options = {
+  color: '#2196f3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.1s',
+    opacity: '0.5s',
+    termination: 400
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+};
+Vue.use(VueProgressBar, options);
+
+Vue.config.productionTip = false;
+
+Vue.use(VueResource);
+Vue.use(Vuetify);
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
